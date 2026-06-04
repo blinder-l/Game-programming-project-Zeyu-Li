0dc0d55e1b104c8a2474ab031c89674811daa042
blinder-l <txwd.ch@gmail.com>
2026-06-04 11:21:27 +0800

    Fix multiline interpolated string compile errors in PrototypeBootstrap

--- FourfoldAccord/Assets/Scripts/PrototypeBootstrap.cs
+++ FourfoldAccord/Assets/Scripts/PrototypeBootstrap.cs
@@ -51,42 +51,47 @@ public class PrototypeBootstrap : MonoBehaviour
 
     private void RunRankBasedHandTests(PokerHandEvaluator pokerHandEvaluator)
     {
-        Debug.Log($"Test Pair: {pokerHandEvaluator.Evaluate(new List<PlayingCard>
+        List<PlayingCard> pairTestCards = new List<PlayingCard>
         {
             new PlayingCard(100, Suit.Hearts, Rank.Ace),
             new PlayingCard(101, Suit.Spades, Rank.Ace)
-        }).handType}");
+        };
+        Debug.Log($"Test Pair: {pokerHandEvaluator.Evaluate(pairTestCards).handType}");
 
-        Debug.Log($"Test Two Pair: {pokerHandEvaluator.Evaluate(new List<PlayingCard>
+        List<PlayingCard> twoPairTestCards = new List<PlayingCard>
         {
             new PlayingCard(102, Suit.Hearts, Rank.King),
             new PlayingCard(103, Suit.Spades, Rank.King),
             new PlayingCard(104, Suit.Clubs, Rank.Three),
             new PlayingCard(105, Suit.Diamonds, Rank.Three)
-        }).handType}");
+        };
+        Debug.Log($"Test Two Pair: {pokerHandEvaluator.Evaluate(twoPairTestCards).handType}");
 
-        Debug.Log($"Test Three of a Kind: {pokerHandEvaluator.Evaluate(new List<PlayingCard>
+        List<PlayingCard> threeOfAKindTestCards = new List<PlayingCard>
         {
             new PlayingCard(106, Suit.Hearts, Rank.Queen),
             new PlayingCard(107, Suit.Spades, Rank.Queen),
             new PlayingCard(108, Suit.Clubs, Rank.Queen)
-        }).handType}");
+        };
+        Debug.Log($"Test Three of a Kind: {pokerHandEvaluator.Evaluate(threeOfAKindTestCards).handType}");
 
-        Debug.Log($"Test Full House: {pokerHandEvaluator.Evaluate(new List<PlayingCard>
+        List<PlayingCard> fullHouseTestCards = new List<PlayingCard>
         {
             new PlayingCard(109, Suit.Hearts, Rank.Ten),
             new PlayingCard(110, Suit.Spades, Rank.Ten),
             new PlayingCard(111, Suit.Clubs, Rank.Ten),
             new PlayingCard(112, Suit.Hearts, Rank.Four),
             new PlayingCard(113, Suit.Spades, Rank.Four)
-        }).handType}");
+        };
+        Debug.Log($"Test Full House: {pokerHandEvaluator.Evaluate(fullHouseTestCards).handType}");
 
-        Debug.Log($"Test Four of a Kind: {pokerHandEvaluator.Evaluate(new List<PlayingCard>
+        List<PlayingCard> fourOfAKindTestCards = new List<PlayingCard>
         {
             new PlayingCard(114, Suit.Hearts, Rank.Nine),
             new PlayingCard(115, Suit.Spades, Rank.Nine),
             new PlayingCard(116, Suit.Clubs, Rank.Nine),
             new PlayingCard(117, Suit.Diamonds, Rank.Nine)
-        }).handType}");
+        };
+        Debug.Log($"Test Four of a Kind: {pokerHandEvaluator.Evaluate(fourOfAKindTestCards).handType}");
     }
 }
